7a59aa957d9db0b5a785c1c139df56d0f1d3e09c
flybird <bluebird88@users.noreply.github.com>
2025-02-01 18:13:13 +0800

    Update pinggy_ssh_tunnel.sh

--- pinggy_ssh_tunnel.sh
+++ pinggy_ssh_tunnel.sh
@@ -1,7 +1,7 @@
 for i in {1..1000}; do
     echo "Running...No. $i"
     echo "------- date:  `date`" &&  
-    nohup ssh -p 443 -R0:localhost:22 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 upBGj2vgFwv+tcp@a.pinggy.io > ssh_log.txt 2>&1 &
+    sh -p 443 -R0:localhost:22 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 upBGj2vgFwv+tcp@a.pinggy.io > ssh_log.txt 
     echo $! > ssh_tunnel.pid
     cat ./ssh_log.txt
 done
